a0f301a2adaea23e1a32403f3ef60ba0a4078d32
Chris-Mara <cm395@njit.edu>
2019-06-29 20:09:55 -0400

    Added some content to the about page. Edited the footer.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,1 @@
-<div id="copyright text-right">© Copyright 2013 Scotchy Scotch Scotch</div>
+<div id="copyright text-right">Chris Mara's Laravel Site</div>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.default')
 @section('content')
-    i am the about page
+   <p>
+       Welcome to my webpage, My name is Chris Mara, I plan on working in the Information Technologies and specializing
+       in cyber security. I enjoy coding anidroid apps and playing around with simple hardware like the Raspberry Pi.
+       After spending my first year as a CS major, I decided to move down to IT as I wasn't a fan of the theory work and
+       prefered just coding programs. I have trouble understanding stuff I don't have any referrence for, because of this
+       I learn more via examples and hands-on work. In terms of coding experience, I just got into it a year ago. I know
+       HTML, Python, and Java, but plan on learning C++ as well some other scripting languages.
+
+       Like pretty much all the other students in the YWCC I like playing video games in my down time. I listen to chill
+       music almost all the time as it helps me concentrate when I can feel my mind wandering off. In terms of hobbies,
+       I skateboard and snowboard, both of which I got into from my brothers. I also tend to go hiking from time to time,
+       I like going to Sandy Hook to see the old WWII bunkers. When things start feeling routine, I'll play paintball or
+       airsoft depending on who's available.
+   </p>
 @stop
